dfc12b0b445f34bf7d38a0b3bea826cb80768c61
Simenhs <sollihogda@gmail.com>
2022-02-09 08:37:12 +0100

    added projects files from bitbucker repo

---
+++ testing/Powersupply_automatic_tests/Powersupply_automatic_tests/Form1.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace Powersupply_automatic_tests
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonSerial = new System.Windows.Forms.Button();
+            this.isConnectedBlueSmoek = new System.Windows.Forms.RadioButton();
+            this.isConnectedKonradPSU = new System.Windows.Forms.RadioButton();
+            this.isConnectedKoradLoad = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.buttonVoltageTest = new System.Windows.Forms.Button();
+            this.buttonCurrentTest = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // buttonSerial
+            // 
+            this.buttonSerial.Location = new System.Drawing.Point(21, 24);
+            this.buttonSerial.Name = "buttonSerial";
+            this.buttonSerial.Size = new System.Drawing.Size(138, 23);
+            this.buttonSerial.TabIndex = 0;
+            this.buttonSerial.Text = "Connect serial";
+            this.buttonSerial.UseVisualStyleBackColor = true;
+            this.buttonSerial.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // isConnectedBlueSmoek
+            // 
+            this.isConnectedBlueSmoek.AutoCheck = false;
+            this.isConnectedBlueSmoek.AutoSize = true;
+            this.isConnectedBlueSmoek.Location = new System.Drawing.Point(21, 75);
+            this.isConnectedBlueSmoek.Name = "isConnectedBlueSmoek";
+            this.isConnectedBlueSmoek.Size = new System.Drawing.Size(79, 17);
+            this.isConnectedBlueSmoek.TabIndex = 1;
+            this.isConnectedBlueSmoek.TabStop = true;
+            this.isConnectedBlueSmoek.Text = "BlueSmoke";
+            this.isConnectedBlueSmoek.UseVisualStyleBackColor = true;
+            // 
+            // isConnectedKonradPSU
+            // 
+            this.isConnectedKonradPSU.AutoCheck = false;
+            this.isConnectedKonradPSU.AutoSize = true;
+            this.isConnectedKonradPSU.Location = new System.Drawing.Point(21, 98);
+            this.isConnectedKonradPSU.Name = "isConnectedKonradPSU";
+            this.isConnectedKonradPSU.Size = new System.Drawing.Size(78, 17);
+            this.isConnectedKonradPSU.TabIndex = 2;
+            this.isConnectedKonradPSU.TabStop = true;
+            this.isConnectedKonradPSU.Text = "Korad PSU";
+            this.isConnectedKonradPSU.UseVisualStyleBackColor = true;
+            // 
+            // isConnectedKoradLoad
+            // 
+            this.isConnectedKoradLoad.AutoCheck = false;
+            this.isConnectedKoradLoad.AutoSize = true;
+            this.isConnectedKoradLoad.Location = new System.Drawing.Point(21, 121);
+            this.isConnectedKoradLoad.Name = "isConnectedKoradLoad";
+            this.isConnectedKoradLoad.Size = new System.Drawing.Size(80, 17);
+            this.isConnectedKoradLoad.TabIndex = 3;
+            this.isConnectedKoradLoad.TabStop = true;
+            this.isConnectedKoradLoad.Text = "Korad Load";
+            this.isConnectedKoradLoad.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Connected:";
+            // 
+            // buttonVoltageTest
+            // 
+            this.buttonVoltageTest.Location = new System.Drawing.Point(301, 24);
+            this.buttonVoltageTest.Name = "buttonVoltageTest";
+            this.buttonVoltageTest.Size = new System.Drawing.Size(139, 23);
+            this.buttonVoltageTest.TabIndex = 5;
+            this.buttonVoltageTest.Text = "Voltage test";
+            this.buttonVoltageTest.UseVisualStyleBackColor = true;
+            this.buttonVoltageTest.Click += new System.EventHandler(this.buttonVoltageTest_Click);
+            // 
+            // buttonCurrentTest
+            // 
+            this.buttonCurrentTest.Location = new System.Drawing.Point(301, 59);
+            this.buttonCurrentTest.Name = "buttonCurrentTest";
+            this.buttonCurrentTest.Size = new System.Drawing.Size(139, 23);
+            this.buttonCurrentTest.TabIndex = 6;
+            this.buttonCurrentTest.Text = "Current Test";
+            this.buttonCurrentTest.UseVisualStyleBackColor = true;
+            this.buttonCurrentTest.Click += new System.EventHandler(this.buttonCurrentTest_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.buttonCurrentTest);
+            this.Controls.Add(this.buttonVoltageTest);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.isConnectedKoradLoad);
+            this.Controls.Add(this.isConnectedKonradPSU);
+            this.Controls.Add(this.isConnectedBlueSmoek);
+            this.Controls.Add(this.buttonSerial);
+            this.Name = "Form1";
+            this.Text = "Powersupply Tester";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonSerial;
+        private System.Windows.Forms.RadioButton isConnectedBlueSmoek;
+        private System.Windows.Forms.RadioButton isConnectedKonradPSU;
+        private System.Windows.Forms.RadioButton isConnectedKoradLoad;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button buttonVoltageTest;
+        private System.Windows.Forms.Button buttonCurrentTest;
+    }
+}
+
---
+++ testing/Powersupply_automatic_tests/Powersupply_automatic_tests/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+
+namespace Powersupply_automatic_tests
+{
+    public partial class Form1 : Form
+    {
+
+        SerialPort portBlueSmoke;
+        SerialPort portKoradPSU;
+        SerialPort portKoradLoad;
+        public Form1()
+        {
+            InitializeComponent();
+
+            portBlueSmoke = new SerialPort("COM3", 9600, Parity.None, 8, StopBits.One);
+            portKoradPSU = new SerialPort("COM15", 9600, Parity.None, 8, StopBits.One);
+            portKoradLoad = new SerialPort("COM17", 9600, Parity.None, 8, StopBits.One);
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+
+            try
+            {
+                portBlueSmoke.Close();
+                portBlueSmoke.Open();
+                isConnectedBlueSmoek.Checked = true;
+            }
+            catch
+            {
+                isConnectedBlueSmoek.Checked = false;
+            }
+
+            try
+            {
+                portKoradPSU.Close();
+                portKoradPSU.Open();
+                isConnectedKonradPSU.Checked = true;
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                isConnectedKonradPSU.Checked = false;
+            }
+
+            try
+            {
+                portKoradLoad.Close();
+                portKoradLoad.Open();
+                isConnectedKoradLoad.Checked = true;
+            }
+            catch ( Exception ex )
+            {
+                Console.WriteLine(ex.Message);
+                isConnectedKoradLoad.Checked = false;
+            }
+
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            portBlueSmoke.Close();
+            portKoradPSU.Close();
+            portKoradLoad.Close();
+        }
+
+        private void buttonVoltageTest_Click(object sender, EventArgs e)
+        {
+            portKoradLoad.WriteLine(":FUNC RES\n:RES 1000OHM\n:INP ON");
+            portBlueSmoke.WriteLine("ISET 0.1\n");
+            System.Threading.Thread.Sleep(1000);
+
+            for (float v =0.0f; v < 12.01f; v+= 0.5f)
+            {
+                Console.WriteLine("VSET " + v.ToString() + "\n");
+                portBlueSmoke.WriteLine("VSET "+v.ToString() +"\n");
+                System.Threading.Thread.Sleep(1000);
+                portKoradLoad.WriteLine(":MEAS:VOLT?\n");
+                string voltMeasStr = portKoradLoad.ReadLine();
+                float voltMeas = float.Parse(voltMeasStr.Split('V')[0]);
+
+                Console.WriteLine("diff: " + (Math.Abs(v - voltMeas)).ToString());
+                if (Math.Abs(v - voltMeas) > 0.1)
+                {
+                    MessageBox.Show("ERROR to hight at " + v.ToString()+"V");
+                }
+
+            }
+
+
+
+            portBlueSmoke.WriteLine("VSET 0\n");
+            portKoradLoad.WriteLine(":INP OFF\n");
+        }
+
+        private void buttonCurrentTest_Click(object sender, EventArgs e)
+        {
+            
+            portBlueSmoke.WriteLine("VSET 3.3\n");
+            portBlueSmoke.WriteLine("ISET 0\n");
+            portKoradLoad.WriteLine(":RES 0OHM\n:INP ON");
+            System.Threading.Thread.Sleep(1000);
+
+
+            for (float i = 0.0f; i < 1.01f; i += 0.1f)
+            {
+                Console.WriteLine("ISET " + i.ToString() + "\n");
+                portBlueSmoke.WriteLine("iSET " + i.ToString() + "\n");
+                System.Threading.Thread.Sleep(1000);
+                portKoradLoad.WriteLine(":MEAS:CURR?\n");
+                string ampMeasStr = portKoradLoad.ReadLine();
+                float ampMeas = float.Parse(ampMeasStr.Split('A')[0]);
+
+                Console.WriteLine("diff: " + (Math.Abs(i - ampMeas)).ToString());
+                if (Math.Abs(i - ampMeas) > 0.02)
+                {
+                    MessageBox.Show("ERROR to hight at " + i.ToString() + "A");
+                }
+
+            }
+
+
+            portBlueSmoke.WriteLine("VSET 0\n");
+            portKoradLoad.WriteLine(":INP OFF\n");
+        }
+    }
+}
