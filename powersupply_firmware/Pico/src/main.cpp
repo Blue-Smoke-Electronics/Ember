df10ea437d77dfb39703fe4321c7a663416d5836
Eirik Wold Solnør <woldsol@live.no>
2022-10-15 22:55:29 +0200

    main.cpp: fix some whitespace

--- powersupply_firmware/Pico/src/main.cpp
+++ powersupply_firmware/Pico/src/main.cpp
@@ -15,26 +15,26 @@
 #include "Battery.h"
 
 int main() {
-    stdio_init_all();   // allowing printf debung in onoff init
+    stdio_init_all(); // allowing printf debug in onoff init
 
-    // keap device powered on, need to be the frist thin that happens 
+    // keep device powered on, need to be the first thing that happens
     Onoff::Init();
 
     Heartbeat heartbeat = Heartbeat();
-    Analog::Init(); 
-    PSU::Init(); 
+    Analog::Init();
+    PSU::Init();
     CommandLineInterface::Init();
     GUI::Init();
     Battery::Init();
     Knobs::Init();
 
-    int i =0; 
+    int i = 0;
     while (true) {
         heartbeat.Update();
         PSU::Update(); 
         CommandLineInterface::Update();
         GUI::Update();
-        Knobs::Update(); 
-        Battery::Update();    
+        Knobs::Update();
+        Battery::Update();
     }
 }
