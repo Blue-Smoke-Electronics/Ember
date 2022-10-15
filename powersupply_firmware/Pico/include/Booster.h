90d160076eac800b13113c02481eaa953c4fb2a8
Eirik Wold Solnør <woldsol@live.no>
2022-10-15 15:18:17 +0200

    Booster.h: fix whitespace

--- powersupply_firmware/Pico/include/Booster.h
+++ powersupply_firmware/Pico/include/Booster.h
@@ -1,24 +1,22 @@
 #ifndef BOOSTER_H
 #define BOOSTER_H
+
 #include "pico/stdlib.h"
 
 class Booster{
     private:
         static const uint32_t update_freq_us = 500; 
-        
+
         static float targetVoltage; 
         
         static uint32_t update_timer; 
         static float pwm_value; 
         static uint pwm_slice_num;
-        
-        
-
     public: 
-    static void Init();
-    static void SetVoltage(float Voltage_V);
-    static void Update(); 
-    static float GetVoltage();
-
+        static void Init();
+        static void SetVoltage(float Voltage_V);
+        static void Update();
+        static float GetVoltage();
 };
+
 #endif
