8d6e3b180e01849173fed7cf8c476e1468157be0
Eirik Wold Solnør <woldsol@live.no>
2022-10-15 21:26:55 +0200

    Analog.cpp: fix some whitespace

--- powersupply_firmware/Pico/src/Analog.cpp
+++ powersupply_firmware/Pico/src/Analog.cpp
@@ -12,38 +12,38 @@ void Analog::Init(){
     adc_gpio_init(Pcb::output_voltage_sens_pin);
     adc_gpio_init(Pcb::output_current_sens_pin);
 }
+
 float Analog::GetTemp(){
     adc_select_input(Pcb::temprature_adc_channal);
     uint16_t raw_temp_data = adc_read();
-    float voltage = raw_temp_data * 3.3f/(1<<12);
-    float temp = 27-(voltage-0.706)/0.001721;
+    float voltage = raw_temp_data * 3.3f / (1 << 12);
+    float temp = 27 - (voltage - 0.706) / 0.001721;
     return temp; 
 }
+
 float Analog::GetOutputCurrent(){ // output in mA
     adc_select_input(Pcb::output_current_sens_adc_channal);
-    uint16_t raw_output_current =0; 
+    uint16_t raw_output_current = 0;
+    raw_output_current += adc_read();
 
-        raw_output_current += adc_read();
-    
-    
-    float output_current = (raw_output_current) * 3.3f/(1<<12)/2.5f ;
+    float output_current = (raw_output_current) * 3.3f / (1 << 12) / 2.5f;
     output_current -= 0.0043f; // current trought constat current circuit 
-    if (output_current < 0.0f){
-        output_current =0; 
-    }
-    return output_current*1000; 
-    
+    if (output_current < 0.0f)
+        output_current = 0;
+
+    return output_current * 1000;
 }
+
 float Analog::GetOutputVoltage(){
     adc_select_input(Pcb::output_voltage_sens_adc_channal);
     uint16_t raw_output_voltage = adc_read();
-    float output_voltage = raw_output_voltage * 3.3f/(1<<12)*11.0f;
-    if (output_voltage<0.0f){
-        output_voltage =0.0f;  
-    }
-    return output_voltage; 
-    
+    float output_voltage = raw_output_voltage * 3.3f / (1 << 12) * 11.0f;
+    if (output_voltage < 0.0f)
+        output_voltage = 0.0f;
+
+    return output_voltage;
 }
+
 float Analog::GetBoosterVoltage(){
     adc_select_input(Pcb::booster_voltage_sens_adc_channal);
     uint16_t booster_voltage_raw = adc_read(); 
@@ -53,6 +53,7 @@ float Analog::GetBoosterVoltage(){
     }
     return booster_voltage; 
 }
+
 float Analog::GetBatteryVoltage(){
     adc_select_input(Pcb::battery_voltage_sens_adc_channal);
     uint16_t battery_voltage_raw = adc_read(); 
